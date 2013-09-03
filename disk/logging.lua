d87ac2f8d9db063592fb7a73b7e8b915d912687c
fzed51 <fzed51@yahoo.fr>
2013-09-03 08:57:38 +0200

    update Logging
    Réécriture du script pour une exploitation à plus grand échelle. le
    système peu être complété par l'utilisation de canaux d'eau se terminant
    par des entonnoir

--- disk/logging.lua
+++ disk/logging.lua
@@ -1,68 +1,73 @@
--- logging v0.1
--- fzed51
+dofile('advTurtle')
 
--- setup :
--- d = dirt
--- w = water
--- T = turtle
--- s = sapling
--- _ = vide
--- niv 0
--- |d|d|d|d|d|d|d|
--- |d|_|_|_|_|w|d|
--- |d|_|_|_|_|_|d|
--- |d|_|_|d|_|_|d|
--- |d|_|_|_|_|_|d|
--- |d|_|_|_|_|_|d|
--- |d|d|d|d|d|d|d|
--- niv 1
--- |_|_|_|_|_|_|_|
--- |_|_|_|_|_|_|_|
--- |_|_|_|_|_|_|_|
--- |_|_|_|s|_|_|_|
--- |_|_|_|_|_|_|_|
--- |_|T|_|_|_|_|_|
--- |_|_|_|_|_|_|_|   
+item.add('coal', 1, 64)
+item.add('sapling', 2, 64)
+item.add('wood', 3, 1)
 
--- item slot
-coal = 1
-sapling = 2
-
-local function logging()
-	turtle.forward()
-	turtle.forward()
-	turtle.turnRight()
-	turtle.forward()
-	if not turtle.detect() then
-		-- tryPlace(sapling)
+local function timber()
+	local hauteur = 0
+	trySelect(item.wood)
+	while turtle.compare(item.wood) do
+		tryDig()
+		if tryUp() then hauteur = hauteur + 1 end
 	end
-	turtle.up()
-	if turtle.detect() then
-		local elevation = 0
-		tryForward()
-		tryDigDown()
-		while turtle.detectUp() do
-			if tryUp() then elevation = elevation + 1 end
-		end
-		while elevation > 0 do
-			if tryDown() then elevation = elevation - 1 end
-		end
-		turtle.back()
+	while hauteur > 0 do
+		if tryDown() then hauteur = hauteur - 1 end		
 	end
-	
-	if not turtle.detect() then
-		-- tryPlace(sapling)
-	end
-	turtle.back()
-	turtle.turnLeft()
-	turtle.back()
-	turtle.back()
 end
 
-while true do
-	logging()
-	for _ = 1, 60 do
-		turtle.suckDown()
-		os.sleep(1)
+local function control()
+	turnLeft()
+	if not turtle.detect() then
+		tryPlace(item.sapling)
 	end
+	timber()
+	turnBack()
+	if not turtle.detect() then
+		tryPlace(item.sapling)
+	end
+	timber()
+	turnLeft()
+end
+
+local function recuperreSapling()
+	turnRight()
+	turtle.suck()
+	turnBack()
+	turtle.suck()
+	turnRight()
+end
+
+local function isOn()
+	local on = false
+	for _,side in pairs(redstone.getSides()) do
+		on = on or redstone.getInput(side)
+	end
+	return on
+end
+
+local n, nb = 0, 4
+
+while true do
+	if isOn() then
+		for _ = 1,4 do
+			while n < nb do
+				tryForward()
+				tryForward()
+				control()
+				n=n+1
+			end
+			turnBack()
+			while n > 0 do
+				tryForward()
+				recuperreSapling()
+				tryForward()
+				n=n-1
+			end
+			turnRight()
+		end
+	else
+		print('Pause ...')
+	end
+	os.sleep(200)
 end
