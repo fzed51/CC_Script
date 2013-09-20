348ee1ff88be9c30b934eabc7e41ee5f15671cde
fzed51 <fzed51@yahoo.fr>
2013-09-20 09:53:04 +0200

    update param
    ajout des fonctions isInt, isString, isBool
    ajout de addOpt

--- disk/param.lua
+++ disk/param.lua
@@ -35,8 +35,6 @@
 +----------------------------------------------------------------------------+
 ]]--
 
-
-
 local parametres, paramsSupl, options, helpMsg, limit = {}, {}, {}, '', true
 --[[
 structure :
@@ -59,8 +57,68 @@ options {
 	defaut		-- [mix] 
 }
 ]]--
+
+local function isInt( val )
+	if type( val ) == 'number' or tonumber( val ) ~= nil then
+		return true
+	else
+		return false, 'L\'argument n\'est pas un nombre.'
+	end
+end
+local function isString( val )
+	if type( val ) == 'string' or tostring( val ) ~= nil then
+		return true
+	else
+		return false, 'L\'argument n\'est pas une chaine.'
+	end
+end
+local function isBool( val )
+	if type ( val ) == 'boolean' then
+		return true
+	else
+		return false, 'L\'argument n\'est pas une valeur logique.'
+	end
+end
+
 function addOpt(nom, valid, default)
-	-- TODO :
+	local _name, _param, _strValid, _valid, _defaut
+	_name = nom
+	if valid == nil then
+		_param = false
+		_strValid = ''
+		_valid = isBool
+		_defaut = false
+	else
+		_param = true
+		if valid == 'int' then
+			_strValid = valid
+			_valid = isInt
+			_defaut = default or 0
+		elseif valid == 'string' then
+			_strValid = valid
+			_valid = isString
+			_defaut = default or ''
+		elseif type(valid) == 'function' then
+			_strValid = 'mix'
+			_valid = valid
+			_defaut = default
+		else
+			print('Option : '.. _name, 'Le 2eme argument de la fonction addOpt doit être \'int\' / \'string\' / une fonction de test avec 1 argument et renvoyer true / false.')
+			error()
+		end
+	end
+	local test, erreur = _valid( _defaut )
+	if not test then
+		print('Option : '.. _name, 'La valeur par defaut n\'est pas valide!', erreur)
+		error()
+	end
+	options[#options + 1] = {
+		name = _name,
+		strValid = _strValid,
+		valid = valid,
+		param = _param,
+		defaut = defaut
+	}
 end
 function add(nom, valid, default)
 	-- TODO :
@@ -95,3 +153,18 @@ end
 function get(args)
 	-- TODO :
 end
+
+--addOpt('option1')
+--addOpt('option2', 'int')
+--addOpt('option3', 'int',5)
+--addOpt('option4', 'string')
+--addOpt('option5', 'string','abc')
+--addOpt('option6', function(a)
+--		a = tonumber(a)
+--		if a >= 0 and a < 10 then
+--			return true
+--		else
+--			return false, 'erreur'
+--		end
+--	end, 1)
+--usage()
